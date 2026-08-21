1b2c02d95447cf622471a495871b082aeeba3a50
shinobiphp <ngp.maestro@gmail.com>
2026-08-20 23:01:07 -0400

    fix: make hello capability accept command payload

--- src/Commands/Hello.php
+++ src/Commands/Hello.php
@@ -6,9 +6,12 @@ namespace Codejitsu\Commands;
 
 final class Hello
 {
-    public static function run(mixed ...$arguments): int
+    public static function run(mixed $arguments = []): int
     {
-        $name = $arguments[0] ?? 'shinobi';
+        $name = is_array($arguments)
+            ? ($arguments[0] ?? 'shinobi')
+            : ($arguments ?: 'shinobi');
+
         fwrite(STDOUT, sprintf("Hello, %s!%s", (string) $name, PHP_EOL));
         return 0;
     }
