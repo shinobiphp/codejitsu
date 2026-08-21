9e1ed093fbeff00510febe6ec1e7c2e0caa854ff
shinobiphp <ngp.maestro@gmail.com>
2026-08-20 23:42:49 -0400

    Return command output to CLI

--- src/Commands/Hello.php
+++ src/Commands/Hello.php
@@ -6,13 +6,12 @@ namespace Codejitsu\Commands;
 
 final class Hello
 {
-    public static function run(mixed $arguments = []): int
+    public static function run(mixed $arguments = []): string
     {
         $name = is_array($arguments)
             ? ($arguments[0] ?? 'shinobi')
             : ($arguments ?: 'shinobi');
 
-        fwrite(STDOUT, sprintf("Hello, %s!%s", (string) $name, PHP_EOL));
-        return 0;
+        return sprintf("Hello, %s!%s", (string) $name, PHP_EOL);
     }
 }
