e2d904b9524ab477d07cf29ca24eecd1df9d6baa
shinobiphp <ngp.maestro@gmail.com>
2026-08-22 21:48:51 -0400

    feat: add lightweight Scroll index entries

---
+++ src/Scrolls/IndexEntry.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codejitsu\Scrolls;
+
+use Codejitsu\Uri\Uri;
+
+final readonly class IndexEntry
+{
+    /**
+     * @param array<string> $tags
+     * @param array<string, mixed> $attributes
+     */
+    public function __construct(
+        public string $type,
+        public string $name,
+        public string $version,
+        public string $source,
+        public array $tags,
+        public array $attributes,
+        public Uri $uri,
+    ) {}
+}
