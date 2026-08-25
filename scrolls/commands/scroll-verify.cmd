5449b15ebc3989b4ce313bc0aa91f4f0827f3d2d
shinobiphp <ngp.maestro@gmail.com>
2026-08-24 22:47:25 -0400

    feat: add scroll verify command

---
+++ scrolls/commands/scroll-verify.cmd
@@ -0,0 +1,5 @@
+name: scroll:verify
+description: Verify the signature of one Scroll or every discovered Scroll.
+usage: 'scroll:verify <uri|--all>'
+type: command
+target: Codejitsu\Commands\ScrollLifecycle::verify
