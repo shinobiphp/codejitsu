38f959059fc8bf582a46e1edbce92ac9baf52614
shinobiphp <ngp.maestro@gmail.com>
2026-08-24 22:47:19 -0400

    feat: add scroll seal command

---
+++ scrolls/commands/scroll-seal.cmd
@@ -0,0 +1,5 @@
+name: scroll:seal
+description: Seal one Scroll or every discovered Scroll after signature verification.
+usage: 'scroll:seal <uri|--all>'
+type: command
+target: Codejitsu\Commands\ScrollLifecycle::seal
