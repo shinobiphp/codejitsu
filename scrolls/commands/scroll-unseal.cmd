8f8d5b0ff2aa5cf3e2198a8271d48ab1784465e1
shinobiphp <ngp.maestro@gmail.com>
2026-08-24 22:47:22 -0400

    feat: add scroll unseal command

---
+++ scrolls/commands/scroll-unseal.cmd
@@ -0,0 +1,5 @@
+name: scroll:unseal
+description: Unseal one Scroll or every discovered Scroll while preserving signatures.
+usage: 'scroll:unseal <uri|--all>'
+type: command
+target: Codejitsu\Commands\ScrollLifecycle::unseal
