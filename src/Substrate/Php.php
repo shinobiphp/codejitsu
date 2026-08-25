6680d2d182ec6efaa741f971ae9d86e5020efc85
shinobiphp <ngp.maestro@gmail.com>
2026-08-25 06:40:38 -0400

    feat: sandbox php substrate

--- src/Substrate/Php.php
+++ src/Substrate/Php.php
@@ -6,7 +6,6 @@ namespace Codejitsu\Substrate;
 
 use Codejitsu\ExecutionContext;
 use Codejitsu\Substrate;
-use LogicException;
 
 final class Php implements Substrate
 {
@@ -17,24 +16,6 @@ final class Php implements Substrate
 
     public function execute(string $source, ExecutionContext $context): mixed
     {
-        $source = preg_replace('/^\s*#![^\r\n]*\r?\n/', '', $source, 1);
-        if ($source === null) {
-            throw new LogicException('Unable to prepare PHP source.');
-        }
-
-        $source = preg_replace('/^\s*<\?php\s*/', '', $source, 1, $phpTagCount);
-        if ($source === null) {
-            throw new LogicException('Unable to prepare PHP source.');
-        }
-
-        if ($phpTagCount === 0) {
-            throw new LogicException('PHP source must begin with <?php.');
-        }
-
-        $runner = static function (ExecutionContext $context) use ($source): mixed {
-            return eval($source);
-        };
-
-        return $runner($context);
+        return (new PhpRunner())->execute($source, $context);
     }
 }
