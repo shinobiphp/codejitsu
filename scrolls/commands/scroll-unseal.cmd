99b65df72aafde297d5daba3d1868dbc88660346
shinobiphp <ngp.maestro@gmail.com>
2026-08-24 22:48:19 -0400

    refactor: use scroll lifecycle command namespace

--- scrolls/commands/scroll-unseal.cmd
+++
@@ -1,5 +0,0 @@
-name: scroll:unseal
-description: Unseal one Scroll or every discovered Scroll while preserving signatures.
-usage: 'scroll:unseal <uri|--all>'
-type: command
-target: Codejitsu\Commands\ScrollLifecycle::unseal
