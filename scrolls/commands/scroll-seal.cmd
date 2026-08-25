7425dd560a97e2a999692a36c96eef19c4e63a4f
shinobiphp <ngp.maestro@gmail.com>
2026-08-24 22:48:15 -0400

    refactor: use scroll lifecycle command namespace

--- scrolls/commands/scroll-seal.cmd
+++
@@ -1,5 +0,0 @@
-name: scroll:seal
-description: Seal one Scroll or every discovered Scroll after signature verification.
-usage: 'scroll:seal <uri|--all>'
-type: command
-target: Codejitsu\Commands\ScrollLifecycle::seal
