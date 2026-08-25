1484542f100a79984e5a7f9855888e00e5224d52
shinobiphp <ngp.maestro@gmail.com>
2026-08-25 06:41:24 -0400

    test: cover wasm substrate

---
+++ tests/Substrate/WasmTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codejitsu\Tests\Substrate;
+
+use Codejitsu\ExecutionContext;
+use Codejitsu\Substrate\Wasm;
+use PHPUnit\Framework\TestCase;
+
+final class WasmTest extends TestCase
+{
+    public function testExecutesWasmModule(): void
+    {
+        $wasmtime = trim((string) shell_exec('command -v wasmtime'));
+        if ($wasmtime === '') {
+            self::markTestSkipped('Wasmtime is not installed.');
+        }
+
+        $module = 'AGFzbQEAAAABBQFgAAF/AwIBAAcHAQNydW4AAAoGAQQAQQNL';
+
+        self::assertSame(3, (new Wasm())->execute($module, new ExecutionContext()));
+    }
+}
