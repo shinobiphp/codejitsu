ae00474540a1daf7e3d043616165ab8a1946097c
shinobiphp <ngp.maestro@gmail.com>
2026-08-27 00:10:56 -0400

    feat: validate package command inputs

--- scrolls/commands/pkg.cmd
+++ scrolls/commands/pkg.cmd
@@ -4,20 +4,23 @@ description: Manage Composer packages used by Codejitsu.
 usage: 'pkg:<subcommand> [package]'
 commands:
     list:
-        description: List installed Composer requirements.
+        description: List Composer requirements.
         usage: 'pkg:list'
         capability: capability://pkg-list
     info:
         description: Show Composer metadata for a package.
         usage: 'pkg:info <package>'
+        schema: schema://package
         capability: capability://pkg-info
     install:
         description: Install a Composer package.
         usage: 'pkg:install <package>'
+        schema: schema://package
         capability: capability://pkg-install
     remove:
         description: Remove a Composer package.
         usage: 'pkg:remove <package>'
+        schema: schema://package
         capability: capability://pkg-remove
     update:
         description: Update a Composer package or the project.
